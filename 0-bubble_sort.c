81a308e418a507708da7cfdc9c133d215e68d27f
Von-sledge <papaamoah568@gmail.com>
2024-01-29 07:48:18 +0000

    sorting algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,17 +9,18 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int n;
+
 	if (size < 2)
 		return;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < size - i - 1; j++)
+		for (j = 1; j < size - i; j++)
 		{
-			if (array[j] > array[j - 1])
+			if (array[j - 1] > array[j])
 			{
 				n = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = n;
+				array[j] = array[j - 1];
+				array[j - 1] = n;
 				print_array(array, size);
 			}
 		}
